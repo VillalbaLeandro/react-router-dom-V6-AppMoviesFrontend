9de3c60868a2e4b6350e9fed1c5f4a81bdc2ed77
VillalbaLeandro <exemprary@gmail.com>
2023-05-14 04:05:08 -0300

    implementacion de react-router-version 6. Agregando la caracteristica SPA

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+function NotFound() {
+    return (
+        <>
+        <div>
+
+          <h1 className="h3 mb-0 text-gray-800"> 404 Not Found</h1>
+        </div>
+        </>
+    )
+}
+
+export default NotFound;
